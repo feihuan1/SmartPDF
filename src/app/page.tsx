b739d163b4ed787c67ce43b4380d744e5d83cbaa
eric <you@example.comericrealtor1997@gmail.com>
2023-09-21 04:37:21 -0700

    authetication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,46 @@
 import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { UserButton,auth } from '@clerk/nextjs'
+import Link from "next/link";
+import { LogIn } from 'lucide-react';
 
-export default function Home() {
+export default async function Home() {
+
+  const { userId } = await auth();
+  const isAuth = !!userId
+
+
+  
   return (
-   <Button>Click me</Button>
+  <div className='w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100 select-none'>
+      <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+          <div className='flex flex-col items-center text-center'>
+            <div className='flex items-center'>
+              <h1 className='mr-3 text-5xl font-semibold'>Smart PDF</h1>
+              <UserButton afterSignOutUrl='/' />
+            </div>
+
+            <div className='flex mt-2'>
+                {isAuth && <Button>Go to Chats →</Button>}
+            </div>
+
+            <p className='max-w-xl mt-1 text-lg text-slate-600'>
+              Join millions of students, researchers and professionals to instantly answer questions and understand research with AI
+
+              <span className='text-xs text-slate-500 block mt-1 '>*Convert any web based ducument, artical or e-book to PDF by click "ctrl + p"</span>
+            </p>
+
+            <div className='w-full mt-4'>
+              {isAuth? (<h1>upload</h1>): (
+                <Link href='/sign-in'>
+                <Button>
+                  Login
+                  <LogIn className='w-4 h-4 ml-2' />
+                  </Button>
+                </Link>
+              )  }
+            </div>
+          </div>
+      </div>
+  </div>
   )
 }
